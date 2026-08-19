db6785ba79e739b72fcfd20dcf389b2dbd59a062
Mario <marioygloriana@gmail.com>
2026-08-18 19:26:57 -0600

    Primeros cambios

--- TuCarreraSV.cs
+++ TuCarreraSV.cs
@@ -12,8 +12,59 @@ namespace TuCarreraSV
         static void Main(string[] args)
         {
             Console.Clear();
-            Console.Title ="TuCarreraSV";
+            Console.Title = "TuCarreraSV";
             Console.ReadKey();
+
+            Console.WriteLine("Bienvenido a la plataforma de TuCarreraSV");
+            Console.WriteLine("Esta plataforma te permite registrarte para comenzar a recibir viajes y ganar dinero");
+            Console.WriteLine("1. Registrarse\n 2. Iniciar sesión\n 3. Salir");
+            switch (int.TryParse(Console.ReadLine(), out int opcion) ? opcion : 0)
+            {
+                case 1:
+                Console.WriteLine("Registrarse");
+                Console.WriteLine("Ingrese su nombre:");
+                string nombre = Console.ReadLine()!;
+                Console.WriteLine("Ingrese su usuario:");
+                string usuario = Console.ReadLine()!;
+                Console.WriteLine("Vuelva a ingresar su usuario:");
+                string usuarioConfirmacion = Console.ReadLine()!;
+                if (usuario != usuarioConfirmacion)
+                {
+                    Console.WriteLine("Los usuarios no coinciden");
+                    break;
+                }
+                break;
+
+                case 2:
+                Console.WriteLine("Iniciar sesión");
+                break;
+
+                case 3:
+                Console.WriteLine("Salir");
+                break;
+            }
+
+        }
+
+
+        }
+
+        class Conductor
+        {
+            string nombre { get; set; }
+            int edad { get; set; }
+            string licencia { get; set; }
+            bool estadoLicencia { get; set; }
+
+            Vehiculo vehiculo { get; set; }
+        }
+
+        class Vehiculo
+        {
+            string marca { get; set; }
+            string modelo { get; set; }
+            int anio { get; set; }
+            string color { get; set; }
+            string placa { get; set; }
         }
     }
-}
