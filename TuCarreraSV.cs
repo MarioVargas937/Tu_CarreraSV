3d913b2c67366dee6805e4af5d72dcb1b0254321
Mario <marioygloriana@gmail.com>
2026-08-18 18:30:23 -0600

    INICIO

---
+++ TuCarreraSV.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TuCarreraSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Title ="TuCarreraSV";
+            Console.ReadKey();
+        }
+    }
+}
